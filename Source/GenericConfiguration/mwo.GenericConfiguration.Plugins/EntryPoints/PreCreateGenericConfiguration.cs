5a0413ced46f45c1e7b290b8244283b60d4e6149
Marius Wodtke <marius.wodtke@avanade.com>
2020-01-01 14:21:05 +0100

    Inital Project with early bounds.

---
+++ Source/GenericConfiguration/mwo.GenericConfiguration.Plugins/EntryPoints/PreCreateGenericConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xrm.Sdk;
+using mwo.GenericConfiguration.Plugins.Models.CRM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mwo.GenericConfiguration.Plugins.EntryPoints
+{
+    [CrmPluginRegistration(MessageNameEnum.Create, mwo_GenericConfiguration.EntityLogicalName, StageEnum.PreOperation, ExecutionModeEnum.Synchronous, "",
+        "mwo.GenericConfiguration.Plugins.Entrypoints.PreCreateGenericConfiguration", 1, IsolationModeEnum.Sandbox)]
+    class PreCreateGenericConfiguration : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
