f7f429cee56aeb22103edbb2648d42bb6b3a78fb
Marius Wodtke <marius.wodtke@avanade.com>
2020-04-05 16:43:50 +0200

    Reader Class to help interaction with the class

---
+++ Source/GenericConfiguration/mwo.GenericConfiguration.Samples.Tests/GenericConfigurationReaderTests.cs
@@ -0,0 +1,189 @@
+﻿using FakeXrmEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mwo.GenericConfiguration.Plugins.Models.CRM;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
+
+namespace mwo.GenericConfiguration.Samples.Tests
+{
+    public class XmlSerializable
+    {
+        public string Name;
+    }
+
+    [TestClass]
+    public class GenericConfigurationReaderTests
+    {
+        private readonly string NotPersistedKey = nameof(NotPersistedKey);
+        private readonly string DefaultKey = nameof(DefaultKey);
+        private readonly string DefaultValue = nameof(DefaultValue);
+        private readonly string NumberKey = nameof(NumberKey);
+        private readonly double NumberValue = 10.0;
+        private readonly string BooleanKey = nameof(BooleanKey);
+        private readonly bool BooleanValue = true;
+        private readonly string JSONKey = nameof(JSONKey);
+        private readonly Dictionary<string, string> JSONValue = new Dictionary<string, string> { { nameof(JSONKey), nameof(JSONValue) } };
+        private readonly string XMLKey = nameof(XMLKey);
+        private readonly XmlSerializable XMLValue = new XmlSerializable { Name = nameof(XMLValue) };
+        private readonly string SemicolonListKey = nameof(SemicolonListKey);
+        private readonly List<int> SemicolonListValue = new List<int> { 1, 2, 3 };
+        private readonly string CommaListKey = nameof(CommaListKey);
+        private readonly List<string> CommaListValue = new List<string> { "a", "b", "c" };
+
+        private CrmServiceContext Context;
+        private GenericConfigurationReader Reader;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var xmlSerializer = new XmlSerializer(XMLValue.GetType());
+            var writer = new StringWriter();
+            xmlSerializer.Serialize(writer, XMLValue);
+
+            var fakectx = new XrmFakedContext();
+            Context = new CrmServiceContext(fakectx.GetOrganizationService());
+
+            Context.AddObject(new mwo_GenericConfiguration { mwo_Key = DefaultKey, mwo_Value = DefaultValue, mwo_TypeEnum = mwo_GenericConfiguration_mwo_Type.Unspecified });
+            Context.AddObject(new mwo_GenericConfiguration { mwo_Key = NumberKey, mwo_Value = NumberValue.ToString(), mwo_TypeEnum = mwo_GenericConfiguration_mwo_Type.Number });
+            Context.AddObject(new mwo_GenericConfiguration { mwo_Key = BooleanKey, mwo_Value = BooleanValue.ToString(), mwo_TypeEnum = mwo_GenericConfiguration_mwo_Type.Boolean });
+            Context.AddObject(new mwo_GenericConfiguration { mwo_Key = JSONKey, mwo_Value = new JavaScriptSerializer().Serialize(JSONValue), mwo_TypeEnum = mwo_GenericConfiguration_mwo_Type.JSON });
+            Context.AddObject(new mwo_GenericConfiguration { mwo_Key = XMLKey, mwo_Value = writer.ToString(), mwo_TypeEnum = mwo_GenericConfiguration_mwo_Type.XML });
+            Context.AddObject(new mwo_GenericConfiguration { mwo_Key = SemicolonListKey, mwo_Value = string.Join(";", SemicolonListValue), mwo_TypeEnum = mwo_GenericConfiguration_mwo_Type.SemicolonseparatedList });
+            Context.AddObject(new mwo_GenericConfiguration { mwo_Key = CommaListKey, mwo_Value = string.Join(",", CommaListValue), mwo_TypeEnum = mwo_GenericConfiguration_mwo_Type.CommaseparatedList });
+            Context.SaveChanges();
+
+            Reader = new GenericConfigurationReader(Context, fakectx.GetFakeTracingService());
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            new CacheManager(nameof(GenericConfigurationReader), TimeSpan.MinValue).Clear();
+        }
+
+        [TestMethod]
+        public void GCReader_GetStringTest()
+        {
+            //Act
+            var result = Reader.GetString(DefaultKey, string.Empty);
+
+            //Assert
+            Assert.AreEqual(DefaultValue, result);
+        }
+
+        [TestMethod]
+        public void GCReader_GetStringDefaultTest()
+        {
+            //Act
+            var result = Reader.GetString(NotPersistedKey, DefaultValue);
+
+            //Assert
+            Assert.AreEqual(DefaultValue, result);
+        }
+
+        [TestMethod]
+        public void GCReader_GetBoolTest()
+        {
+            //Act
+            var result = Reader.GetBool(BooleanKey, null);
+
+            //Assert
+            Assert.AreEqual(BooleanValue, result);
+        }
+
+        [TestMethod]
+        public void GCReader_GetBoolgDefaultTest()
+        {
+            //Act
+            var result = Reader.GetBool(NotPersistedKey, BooleanValue);
+
+            //Assert
+            Assert.AreEqual(BooleanValue, result);
+        }
+
+        [TestMethod]
+        public void GCReader_GetNumberTest()
+        {
+            //Act
+            var result = Reader.GetNumber(NumberKey, double.NaN);
+
+            //Assert
+            Assert.AreEqual(NumberValue, result);
+        }
+
+        [TestMethod]
+        public void GCReader_GetNumberDefaultTest()
+        {
+            //Act
+            var result = Reader.GetNumber(NotPersistedKey, NumberValue);
+
+            //Assert
+            Assert.AreEqual(NumberValue, result);
+        }
+
+        [TestMethod]
+        public void GCReader_GetListCommaTest()
+        {
+            //Act
+            var result = Reader.GetList(CommaListKey, null);
+
+            //Assert
+            Assert.AreEqual(CommaListValue.First(), result.First());
+        }
+
+        [TestMethod]
+        public void GCReader_GetListSemicolonTest()
+        {
+            //Act
+            var result = Reader.GetList(SemicolonListKey, null);
+
+            //Assert
+            Assert.AreEqual(SemicolonListValue.First().ToString(), result.First());
+        }
+
+        [TestMethod]
+        public void GCReader_GetListDefaultValueTest()
+        {
+            //Act
+            var result = Reader.GetList(NotPersistedKey, CommaListValue);
+
+            //Assert
+            Assert.AreEqual(CommaListValue.First(), result.First());
+        }
+
+        [TestMethod]
+        public void GCReader_GetJSONTest()
+        {
+            //Act
+            var result = Reader.GetObject<Dictionary<string,string>>(JSONKey, null);
+
+            //Assert
+            Assert.AreEqual(JSONValue.Keys.First(), result.Keys.First());
+        }
+
+
+        [TestMethod]
+        public void GCReader_GetXmlTest()
+        {
+            //Act
+            var result = Reader.GetObject<XmlSerializable>(XMLKey, null);
+
+            //Assert
+            Assert.AreEqual(XMLValue.Name, result.Name);
+        }
+
+        [TestMethod]
+        public void GCReader_GetObjectNotExistingTest()
+        {
+            //Act
+            var result = Reader.GetObject(NotPersistedKey, JSONValue);
+
+            //Assert
+            Assert.AreEqual(JSONValue.Keys.First(), result.Keys.First());
+        }
+    }
+}
---
+++ Source/GenericConfiguration/mwo.GenericConfiguration.Samples/GenericConfigurationReader.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.Xrm.Sdk;
+using mwo.GenericConfiguration.Plugins.Models.CRM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
+
+namespace mwo.GenericConfiguration.Samples
+{
+    /// <summary>
+    /// Class will help interacting with the CRM Entity mwo_genericconfiguration which holds configuration entries for custom logic.
+    /// </summary>
+    /// <remarks>
+    /// Facilitates Caching to improve performance, configuration changes might be delayed.
+    /// </remarks>
+    public class GenericConfigurationReader
+    {
+        /// <summary>
+        /// Change this duration if you feel that the default duration is to long/short. 
+        /// You could make this configurable as well.
+        /// </summary>
+        public TimeSpan DefaultCacheDuration { get; } = TimeSpan.FromMinutes(10);
+
+        private readonly ITracingService Trace;
+        private IQueryable<mwo_GenericConfiguration> Query { get; set; }
+        private readonly CrmServiceContext Context;
+        private readonly ICache Cache;
+        private readonly Func<mwo_GenericConfiguration, mwo_GenericConfiguration> Selector = _ => new mwo_GenericConfiguration { Id = _.Id, mwo_Key = _.mwo_Key, mwo_Value = _.mwo_Value, mwo_TypeEnum = _.mwo_TypeEnum } ;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="trace"></param>
+        public GenericConfigurationReader(CrmServiceContext context, ITracingService trace)
+        {
+            Context = context;
+            Trace = trace;
+            Query = Context.mwo_GenericConfigurationSet.Where(_ => _.StateCode == mwo_GenericConfigurationState.Active);
+            Cache = new CacheManager(nameof(GenericConfigurationReader), DefaultCacheDuration);
+        }
+
+        /// <summary>
+        /// Retrieves the given Key and returns its value.
+        /// </summary>
+        /// <param name="key">Corrensponds to the mwo_Key field for retrieval</param>
+        /// <param name="defaultValue">is returned when the Key is unconfigured</param>
+        /// <returns>mwo_Value, if Key was not found, returns defaultValue</returns>
+        public string GetString(string key, string defaultValue)
+        {
+            return Get(key)?.mwo_Value ?? defaultValue;
+        }
+
+        /// <summary>
+        /// Retrieves the given Key and converts it to a List.
+        /// </summary>
+        /// <remarks>
+        /// mwo_Type determines the sepator.
+        /// </remarks>
+        /// <param name="key">Corrensponds to the mwo_Key field for retrieval</param>
+        /// <param name="defaultValue">is returned when the Key is unconfigured</param>
+        /// <returns>mwo_Value converted to a List, if Key was not found, returns defaultValue</returns>
+        public IEnumerable<string> GetList(string key, IEnumerable<string> defaultValue)
+        {
+            var config = Get(key);
+            if (config == null || string.IsNullOrEmpty(config.mwo_Value)) return defaultValue;
+            var separator = config.mwo_TypeEnum == mwo_GenericConfiguration_mwo_Type.SemicolonseparatedList ? ';' : ',';
+
+            return config.mwo_Value.Split(separator).ToList();
+        }
+
+        /// <summary>
+        /// Retrieves the given Key and converts it to a Boolean.
+        /// </summary>
+        /// <param name="key">Corrensponds to the mwo_Key field for retrieval</param>
+        /// <param name="defaultValue">is returned when the Key is unconfigured</param>
+        /// <returns>mwo_Value converted to a Boolean, if Key was not found, returns defaultValue</returns>
+        public bool? GetBool(string key, bool? defaultValue)
+        {
+            var config = Get(key);
+            var  success = bool.TryParse(config?.mwo_Value, out bool value);
+            return success ? value : defaultValue;
+        }
+
+        /// <summary>
+        /// Retrieves the given Key and converts it to a Double.
+        /// </summary>
+        /// <param name="key">Corrensponds to the mwo_Key field for retrieval</param>
+        /// <param name="defaultValue">is returned when the Key is unconfigured</param>
+        /// <returns>mwo_Value converted to a Double, if Key was not found, returns defaultValue</returns>
+        public double GetNumber(string key, double defaultValue)
+        {
+            var config = Get(key);
+            var success = double.TryParse(config?.mwo_Value, out double value);
+            return success ? value : defaultValue;
+        }
+
+        /// <summary>
+        /// Retrieves the given Key and converts it to an object of the specified type.
+        /// </summary>
+        /// <remarks>
+        /// mwo_Type determines wether JSON or XML serialization is used. If mwo_Type does not have an appropriate value, the type is guessed from the content.
+        /// </remarks>
+        /// <typeparam name="TOut">The Type that is expected in the Configuration, e.g. Dictionary<string,string></typeparam>
+        /// <param name="key">Corrensponds to the mwo_Key field for retrieval</param>
+        /// <param name="defaultValue">is returned when the Key is unconfigured</param>
+        /// <returns>mwo_Value converted to the specified object, if Key was not found or deserilization is not possible, returns defaultValue</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "defaultValue will be returned")]
+        public TOut GetObject<TOut>(string key, TOut defaultValue)
+        {
+            var config = Get(key);
+            if (config == null || string.IsNullOrEmpty(config.mwo_Value)) return defaultValue;
+
+            try
+            {
+                if (config.mwo_TypeEnum == mwo_GenericConfiguration_mwo_Type.JSON || config.mwo_Value.StartsWith("{") || config.mwo_Value.StartsWith("["))
+                {
+                    var serializer = new JavaScriptSerializer();
+                    return serializer.Deserialize<TOut>(config.mwo_Value);
+                }
+                else if (config.mwo_TypeEnum == mwo_GenericConfiguration_mwo_Type.XML || config.mwo_Value.StartsWith("<"))
+                {
+                    using (var reader = new System.IO.StringReader(config.mwo_Value))
+                    {
+                        var serializer = new XmlSerializer(typeof(TOut));
+                        return (TOut)serializer.Deserialize(reader);
+                    }
+                }
+            } 
+            catch (Exception ex)
+            {
+                Trace?.Trace($"Failed to generate Object: {ex.Message}\n{ex.StackTrace}");
+            }
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Internal retrieval method.
+        /// </summary>
+        /// <remarks>
+        /// This method faicilates caching to improve performance.
+        /// Unconfigured calues are not cached.
+        /// Not all fields are returned.
+        /// </remarks>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private mwo_GenericConfiguration Get(string key)
+        {
+            if (Cache.Has(key)) return Cache.Get<mwo_GenericConfiguration>(key);
+
+            Trace?.Trace("No Config in Cache, retrieving.");
+            var record = Query.Where(_ => _.mwo_Key.Equals(key)).Select(Selector).FirstOrDefault();
+
+            if (record != null) Cache.Set(key, record);
+            else Trace?.Trace("No Config in CRM.");
+
+            return record;
+        }
+    }
+}
